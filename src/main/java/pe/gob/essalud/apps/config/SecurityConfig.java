7410f9e0e04bdd7322b70da76a0391b711691a6d
Isaac <isaacac19@gmail.com>
2023-05-03 23:37:31 -0500

    Deshabilita proteccion para endpoints de swagger

--- src/main/java/pe/gob/essalud/apps/config/SecurityConfig.java
+++ src/main/java/pe/gob/essalud/apps/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable() //Cross-Site Request Forgery disable to API
+                .csrf().ignoringAntMatchers("/swagger-ui/**","/v3/api-docs/**").disable() //Cross-Site Request Forgery disable to API
                 .httpBasic() // login with Auth Basic for getting a login
                 .authenticationEntryPoint(new Http401AuthenticationEntryPoint())
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // stateless to API
