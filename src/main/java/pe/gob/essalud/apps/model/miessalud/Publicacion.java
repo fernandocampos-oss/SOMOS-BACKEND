d750d6db61aeaec7b3a41678b1fc9bf572bcdc3f
Isaac <isaacac19@gmail.com>
2023-05-20 22:59:39 -0500

    Establece fecha actual de creacion y modificacion de publicacion

--- src/main/java/pe/gob/essalud/apps/model/miessalud/Publicacion.java
+++ src/main/java/pe/gob/essalud/apps/model/miessalud/Publicacion.java
@@ -2,12 +2,11 @@ package pe.gob.essalud.apps.model.miessalud;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Getter
 @Setter
@@ -33,13 +32,21 @@ public class Publicacion {
     private boolean esActivo;
     @Column(name = "usuario_creacion", nullable = false)
     private Integer usuarioCreacion;
-    @CreationTimestamp
     @Column(name = "fecha_creacion", nullable = false)
     private LocalDateTime fechaCreacion;
     @Column(name = "usuario_modificacion")
     private Integer usuarioModificacion;
-    @UpdateTimestamp
     @Column(name = "fecha_modificacion")
     private LocalDateTime fechaModificacion;
+
+    @PrePersist
+    private void prePersist() {
+        this.fechaCreacion = LocalDateTime.now(ZoneId.of("America/Lima"));
+    }
+
+    @PreUpdate
+    private void preUpdate() {
+        this.fechaModificacion = LocalDateTime.now(ZoneId.of("America/Lima"));
+    }
 
 }
