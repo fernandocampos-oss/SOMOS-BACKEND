9b5c5b1eb34b8f5c04367b581ede3b42a4440a92
Ericks <evillantoyh@hotmail.com>
2025-02-18 15:26:24 -0500

    Test-1 envio horario

--- src/main/java/pe/gob/essalud/apps/controller/OnomasticoController.java
+++ src/main/java/pe/gob/essalud/apps/controller/OnomasticoController.java
@@ -72,7 +72,7 @@ public class OnomasticoController extends BaseController {
             }
         };
 //		long initialDelay = calculateInitialDelay(7, 0); // Hora: 7:00 AM formato 24h
-        long initialDelay = calculateInitialDelay(15, 10);
+        long initialDelay = calculateInitialDelay(15, 42);
         long period = TimeUnit.DAYS.toMillis(1); // Repeticion por cada día
         scheduler.scheduleAtFixedRate(task, initialDelay, period, TimeUnit.MILLISECONDS);
 
