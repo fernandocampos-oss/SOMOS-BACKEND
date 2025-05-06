0ee2f848da8dd5a5ad3c9a6635bac5596c396173
Isaac <isaacac19@gmail.com>
2025-05-06 17:11:08 -0500

    Envia ip de cliente en validacion recaptcha

--- src/main/java/pe/gob/essalud/apps/client/RecaptchaEnterpriseServiceClient.java
+++ src/main/java/pe/gob/essalud/apps/client/RecaptchaEnterpriseServiceClient.java
@@ -2,6 +2,7 @@ package pe.gob.essalud.apps.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import pe.gob.essalud.apps.config.FormUrlEncodedFeignConfig;
 import pe.gob.essalud.apps.dto.recaptcha.RecaptchaResponseDto;
@@ -11,7 +12,7 @@ import pe.gob.essalud.apps.dto.recaptcha.RecaptchaResponseDto;
 public interface RecaptchaEnterpriseServiceClient {
 
     @PostMapping(value = "verify", consumes = "application/x-www-form-urlencoded")
-    RecaptchaResponseDto verify(@RequestParam("token") String token, @RequestParam("action") String action);
+    RecaptchaResponseDto verify(@RequestParam("token") String token, @RequestParam("action") String action, @RequestHeader("X-Client-IP") String clientIp);
 
 }
 
