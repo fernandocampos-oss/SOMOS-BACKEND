2c5a8bfd06507bc8b69f10d208c065101aedf696
Isaac <isaacac19@gmail.com>
2026-03-13 11:34:19 -0500

    Deshabilita restricciones

--- src/main/java/pe/gob/essalud/apps/service/impl/MaterialServiceImpl.java
+++ src/main/java/pe/gob/essalud/apps/service/impl/MaterialServiceImpl.java
@@ -6,7 +6,6 @@ import pe.gob.essalud.apps.client.MaterialSapServiceClient;
 import pe.gob.essalud.apps.dto.material.MaestroMaterialSAP;
 import pe.gob.essalud.apps.dto.material.StockMaterialSAP;
 import pe.gob.essalud.apps.service.MaterialService;
-import pe.gob.essalud.apps.service.UsuarioService;
 
 import java.util.List;
 
@@ -15,21 +14,14 @@ import java.util.List;
 public class MaterialServiceImpl implements MaterialService {
 
     private final MaterialSapServiceClient materialSapServiceClient;
-    private final UsuarioService usuarioService;
 
     @Override
     public List<MaestroMaterialSAP> buscarMaestroMaterial(String nombreMaterial) {
-        if (usuarioService.usuarioTienePermisoModulo("MATERIALES")) {
-            return materialSapServiceClient.getMaestroMaterialSAP(nombreMaterial);
-        }
-        return List.of();
+        return materialSapServiceClient.getMaestroMaterialSAP(nombreMaterial);
     }
 
     @Override
     public List<StockMaterialSAP> buscarStockMaterial(String codigoMaterial, String codigoRed) {
-        if (usuarioService.usuarioTienePermisoModulo("MATERIALES")) {
-            return materialSapServiceClient.getStockMaterialSAP(codigoMaterial, codigoRed);
-        }
-        return List.of();
+        return materialSapServiceClient.getStockMaterialSAP(codigoMaterial, codigoRed);
     }
 }
